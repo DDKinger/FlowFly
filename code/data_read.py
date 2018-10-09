ff79862b25c68cb412f0b418f5cffa878f06a355
DDKinger <ddkingzhang@gmail.com>
2018-10-09 11:35:06 +0800

    data_read

--- code/data_read.py
+++ code/data_read.py
@@ -1,10 +1,11 @@
 import os
+import numpy as np
 
 def load_data(data_dir, data_gaussian, is_training):
     if data_gaussian:
-        data_name = 'TrafficFlow_69_gaussian'
+        data_name = 'TrafficFlow_69_12week_6day_gaussian'
     else:
-        data_name = 'TrafficFlow_69_normal'
+        data_name = 'TrafficFlow_69_12week_6day_normal'
     if is_training:
         data_name += '_train.mat'
     else:
@@ -14,8 +15,23 @@ def load_data(data_dir, data_gaussian, is_training):
 def generate_next(x, batch_size, time_step):
     bs = batch_size
     ts = time_step
-    for i in range(0, len(x)-bs, bs):
-        yield x[]
+    n_weekday = 1440
+    n_week = 1728
+    _x = []
+    _y = []
+    _bs = 0
+    for i in range(0, len(x)-n_week+1, n_week):
+        for j in range(i, i+n_weekday-ts+1):
+            _x.append(x[j:j+ts])
+            _y.append(x[j+ts])
+            _bs += 1
+            if _bs == bs:
+                _x = np.array(_x)
+                _y = np.array(_y)
+                yield _x, _y
+                _x = []
+                _y = []
+
 
 
 # def shuffle(x):
